0c8aa94c3d603f22e38934b5dc8d8d9b62fc008c
= <=>
2026-08-08 21:50:45 +0100

    Change wepon explodes

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -16,7 +16,7 @@ extends CharacterBody2D
 
 
 func _ready() -> void:
-	global_position += transform.x * 24
+	global_position += transform.x * 20
 	velocity += transform.x * speed
 	explosion_timer.start(explosion_time)
 
@@ -28,14 +28,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-func explode():
+func explode(big_explode: bool = false):
+	projectile_hp -= 1
+	if projectile_hp <= 0 or big_explode:
+		big_explode = true
+		queue_free()
+	
 	# Shake the screen
 	Mouse.shake()
 	
-	var hit_particle: GPUParticles2D = load("res://scenes/hit_particle.tscn").instantiate()
-	hit_particle.global_position = global_position
-	hit_particle.emitting = true
-	get_parent().add_child(hit_particle)
+	if big_explode:
+		var hit_particle: GPUParticles2D = load("res://scenes/hit_particle.tscn").instantiate()
+		hit_particle.global_position = global_position
+		hit_particle.emitting = true
+		get_parent().add_child(hit_particle)
 	
 	# destroy map
 	var tile_position: Vector2 = round(global_position / 4.0)
@@ -44,27 +50,23 @@ func explode():
 			get_parent().remove_tile(tile_position + Vector2(sin(number / explosion_accuracy) * size, cos(number / explosion_accuracy) * size))
 	
 	# hit players
-	global_position -= transform.x * 16
-	
 	for body: Node2D in hurt_box.get_overlapping_bodies():
 		if body is Player:
 			var hit_power: float = clampf(1.0 / global_position.distance_squared_to(body.global_position) * 1000.0, 0.01, 1.0)
-			body.damage(hit_power * damage)
-			body.velocity = -transform.x * hit_power * knockback
-	
-	projectile_hp -= 1
-	if projectile_hp <= 0:
-		queue_free()
+			if big_explode:
+				body.damage(hit_power * damage)
+				body.velocity = -transform.x * hit_power * knockback
+			else:
+				body.damage(hit_power * damage / 2.0)
 
 
 func _on_explosion_timer_timeout() -> void:
-	explode()
-	queue_free()
+	explode(true)
 
 
 func _on_detect_box_body_entered(body: Node2D) -> void:
-	explode()
-	
 	if body is Player:
-		queue_free()
+		explode(true)
+	else:
+		explode()
 	
