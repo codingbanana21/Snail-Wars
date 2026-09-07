7edb31ef04f42834595ab2b6094cf9fbfb449ab6
= <=>
2026-09-07 19:12:21 +0100

    Code clean up

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,7 +18,7 @@ const JUMP: float = -450.0
 const SPEED: float = 12.0
 const JUMP_SPEED: float = 180
 const PLAYER_GRAVITY: float = 30.0
-const WEAPONS: Array[String] = ["rocket", "grenade", "drill", "bomb", "air_strike", "drill_strike", "tnt", "land_mine_strike", "destroyer_of_games"]
+const WEAPONS: Array[String] = ["rocket", "grenade", "drill", "bomb", "air_strike", "drill_strike", "tnt", "destroyer_of_games"]
 
 var projectile_speed: float = 0.0
 var max_hp: float = 100.0
@@ -67,9 +67,9 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("last_weapon"):
 			weapon -= 1
 			if weapon < 0:
-				weapon = 8
+				weapon = 7
 		
-		weapon %= 9
+		weapon %= 8
 		Mouse.weapon_left = str(Globals.teams_weapons[team_number][weapon])
 		Mouse.weapon = weapon
 		
@@ -144,8 +144,10 @@ func next_player():
 	is_player_turn = Globals.player_turn == player_number and team_number == Globals.team_turn
 	
 	if hp <= 0:
-		set_physics_process(false)
-		global_position.x = 100000
+		if is_physics_processing():
+			shot_projectile("res://projectiles/explosion_snail.tscn")
+			set_physics_process(false)
+			global_position.x = 100000
 	elif is_player_turn:
 		Mouse.global_position = global_position
 
--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -4,21 +4,21 @@ extends CharacterBody2D
 @onready var detect_box: Area2D = $DetectBox
 @onready var explosion_timer: Timer = $ExplosionTimer
 
-@export var speed: float = 60.0
-@export var projectile_gravity: int = 500
 @export var damage: float = 45
+@export var size: int = 14
 @export var knockback: int = 1000
-@export var explosion_time: float = 3.0
-@export var no_explosion_time: bool = false
-@export var explosion_size: int = 14
+@export var speed: float = 60.0
+@export var gravity: int = 500
 @export var projectile_hp: int = 1
+@export var timer: float = 3.0
 @export var bounce: bool = false
 @export var spawn_at_mouse: bool = false
+@export var not_players: bool = false
 
 
 func _ready() -> void:
-	if !no_explosion_time:
-		explosion_timer.start(explosion_time)
+	if !not_players:
+		explosion_timer.start(timer)
 	
 	#spawn type
 	if spawn_at_mouse:
@@ -30,10 +30,10 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if !no_explosion_time:
+	if !not_players:
 		Mouse.global_position = global_position
 	
-	velocity.y += projectile_gravity * delta
+	velocity.y += gravity * delta
 	rotation = velocity.angle()
 	move_and_slide()
 
@@ -49,7 +49,7 @@ func explode(end_explode: bool = false):
 		end_explode = true
 		queue_free()
 		
-		if !no_explosion_time:
+		if !not_players:
 			set_physics_process(false)
 			get_parent().next_player_timer.start()
 			get_parent().has_shot_projectile = false
@@ -67,7 +67,7 @@ func explode(end_explode: bool = false):
 	var tile_position: Vector2 = round(global_position / 4.0)
 	var explosion_accuracy: float = PI * 2
 	
-	for size in range(explosion_size):
+	for size in range(size):
 		for number in range(explosion_accuracy * 8 * size):
 			get_parent().get_parent().remove_tile(tile_position + Vector2(sin(number / explosion_accuracy) * size, cos(number / explosion_accuracy) * size))
 	
@@ -75,7 +75,7 @@ func explode(end_explode: bool = false):
 	for player: Player in get_tree().get_nodes_in_group("Player"):
 		var dis_to: float = global_position.distance_to(player.global_position)
 		
-		if dis_to < (4.0 * explosion_size):
+		if dis_to < (4.0 * size):
 			var hit_power: float = clampf(16.0 / dis_to, 0.01, 1.0)
 			
 			player.damage(hit_power * damage)
@@ -87,5 +87,5 @@ func _on_explosion_timer_timeout() -> void:
 
 
 func _on_detect_box_body_entered(body: Node2D) -> void:
-	if explosion_timer.time_left <= explosion_time - 0.05:
+	if explosion_timer.time_left <= timer - 0.05:
 		explode()
